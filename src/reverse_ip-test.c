3d7f8d5f5bb77d251abccde9b3670446cb59f2ad
Dmitry Mikhirev <mikhirev@gmail.com>
2023-02-04 03:14:43 +0300

    fix mistyping in email address

--- src/reverse_ip-test.c
+++ src/reverse_ip-test.c
@@ -1,6 +1,6 @@
 /*
  * Copyright (c) 2023
- *               Dmitry Mikhirev <dmitry@mikhirev@ru>
+ *               Dmitry Mikhirev <dmitry@mikhirev.ru>
  *
  * Permission to use, copy, modify, and distribute this software for any
  * purpose with or without fee is hereby granted, provided that the above

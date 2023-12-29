83baabcf70b9886f4048900f3e6a9b52c531a2ff
Dmitry Mikhirev <mikhirev@gmail.com>
2023-12-30 01:45:37 +0400

    let cmake detect endianness

--- src/lookup3.c
+++ src/lookup3.c
@@ -40,31 +40,18 @@ on 1 byte), but shoehorning those bytes into integers efficiently is messy.
 */
 #include <time.h>		/* defines time_t for timings in the test */
 #include <stdint.h>		/* defines uint32_t etc */
-#include <sys/param.h>		/* attempt to define endianness */
-#ifdef linux
-# include <endian.h>		/* attempt to define endianness */
-#endif
 
 #include <common.h>
 
-/*
- * My best guess at if you are big-endian or little-endian.  This may
- * need adjustment.
- */
-#if (defined(__BYTE_ORDER) && defined(__LITTLE_ENDIAN) && \
-     __BYTE_ORDER == __LITTLE_ENDIAN) || \
-    (defined(i386) || defined(__i386__) || defined(__i486__) || \
-     defined(__i586__) || defined(__i686__) || defined(vax) || defined(MIPSEL))
-# define HASH_LITTLE_ENDIAN 1
-# define HASH_BIG_ENDIAN 0
-#elif (defined(__BYTE_ORDER) && defined(__BIG_ENDIAN) && \
-       __BYTE_ORDER == __BIG_ENDIAN) || \
-      (defined(sparc) || defined(POWERPC) || defined(mc68000) || defined(sel))
-# define HASH_LITTLE_ENDIAN 0
+#ifdef BIG_ENDIAN
 # define HASH_BIG_ENDIAN 1
 #else
-# define HASH_LITTLE_ENDIAN 0
 # define HASH_BIG_ENDIAN 0
+#endif
+#ifdef LITTLE_ENDIAN
+# define HASH_LITTLE_ENDIAN 1
+#else
+# define HASH_LITTLE_ENDIAN 0
 #endif
 
 #define hashsize(n) ((uint32_t)1<<(n))
